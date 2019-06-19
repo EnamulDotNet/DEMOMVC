1d9294fc17c09b854a80c26d0f351e07266345b5
MD. ENAMUL HAQUE <EnamulDotNet@users.noreply.github.com>
2019-06-19 11:33:12 -0700

    all in views

---
+++ DEMOMVC/Views/Test/P1Controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace DEMOMVC.Views.Test
+{
+    public class P1Controller : Controller
+    {
+        //
+        // GET: /P1/
+        public ActionResult Index()
+        {
+            return View("../Test/P1");
+        }
+	}
+}
---
+++ DEMOMVC/Views/Test/P2Controller.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace DEMOMVC.Views.Test
+{
+    public class P2Controller : Controller
+    {
+        //
+        // GET: /P1/
+        public ActionResult Index()
+        {
+            return View("../Test/P3");
+        }
+	}
+}
