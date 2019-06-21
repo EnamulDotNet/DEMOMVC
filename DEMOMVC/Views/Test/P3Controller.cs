fb68692944c534d377acf17c1272403d3e26a60f
MD. ENAMUL HAQUE <EnamulDotNet@users.noreply.github.com>
2019-06-21 12:56:36 -0700

    send master details from view to controller in json object at a time

--- DEMOMVC/Views/Test/P3Controller.cs
+++ DEMOMVC/Views/Test/P3Controller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
@@ -42,7 +43,7 @@ namespace DEMOMVC.Views.Test
             dt.Rows.Add("1", "BD");
             dt.Rows.Add("2", "IN");
             dt.Rows.Add("2", "usa");
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10; i++)
             {
                 dt.Rows.Add(i.ToString(), "fkhfkfsfj jsjdfj hkh kfsf   fsfsfsfsfsf fff");
             }
@@ -52,26 +53,61 @@ namespace DEMOMVC.Views.Test
             dt=new DataTable("dt2");
             dt.Columns.Add("id");
             dt.Columns.Add("name");
-            dt.Columns.Add("city");
-            dt.Columns.Add("address");
-            for (int i = 0; i < 1000; i++)
+            dt.Columns.Add("unit");
+            dt.Columns.Add("qty");
+            for (int i = 0; i < 10; i++)
             {
-                dt.Rows.Add(i.ToString(), "Name" + i, "City" + i, "Aaddress" + i);
+                dt.Rows.Add("Id"+i, "Name" + i, "Unit" + i,  i.ToString());
             }
             ds1.Tables.Add(dt);
 
-
-
-
-
-
-
-
-
-
-
             return Json(JsonConvert.SerializeObject(ds1), JsonRequestBehavior.AllowGet);
 
+        }
+
+        [HttpPost]
+        public JsonResult Save(AllData obj)
+        {
+            DataTable dt = ToDataTable(obj.Dt1);
+            return Json("1", JsonRequestBehavior.AllowGet);
+        }
+        public static DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+
+            //Get all the properties
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                //Defining type of data column gives proper data table 
+                var type = (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) ? Nullable.GetUnderlyingType(prop.PropertyType) : prop.PropertyType);
+                //Setting column names as Property names
+                dataTable.Columns.Add(prop.Name, type);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    //inserting property values to datatable rows
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            //put a breakpoint here and check datatable
+            return dataTable;
+        }
+        public class AllData
+        {
+            public string SuppId { get; set; }
+            public string TrnDat { get; set; }
+            public List<Dt1Data> Dt1 { get; set; }
+
+        }
+        public class Dt1Data
+        {
+            public string ItemId { get; set; }
+            public string Qty { get; set; }
         }
 	}
 }
